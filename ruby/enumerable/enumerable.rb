33b5acd51a5e6fb095b8e396097eb8ef7d02e35c
Punnadit Trussat <punnadittr@gmail.com>
2018-04-05 09:45:13 +0700

    finished all methods

--- ruby/enumerable/enumerable.rb
+++ ruby/enumerable/enumerable.rb
@@ -95,12 +95,16 @@ module Enumerable
         end
         counter
     end
-    def my_map
+    def my_map(proc = nil)
         arr = self.to_a
         new_arr = Array.new
         i = 0
         while i < self.size
-            new_arr << yield(arr[i])
+            if block_given?
+                new_arr << yield(arr[i])
+            else
+                new_arr << proc.call(arr[i])
+            end
             i += 1
         end
         new_arr
@@ -108,17 +112,17 @@ module Enumerable
     def my_inject(arg1 = nil, arg2 = nil)
         arr = self.to_a
         memo = arr[0]
-        if arg2
-            i = 0
-            sym = arg2
-            memo = arg1
+        i = 1
+        if arg2 
+            i = 0 
+            sym = arg2 
+            memo = arg1 
         else 
-            i = 1
-            sym = arg1
+            sym = arg1 
         end
         if block_given? and arg1
-            i = 0
-            memo = arg1
+            i = 0 
+            memo = arg1 
         end
         if block_given?
             while i < self.size
@@ -136,17 +140,51 @@ module Enumerable
 end
    
 =begin
+## Test Suite From Ruby 2.3.4 Docs @ http://ruby-doc.org/ ##
+
+#SELECT
+(1..10).my_select { |i|  i % 3 == 0 }           #=> [3, 6, 9]
+[1,2,3,4,5].my_select { |num|  num.even?  }     #=> [2, 4]
+
+#ALL?
+%w[ant bear cat].my_all? { |word| word.length >= 3 } #=> true
+%w[ant bear cat].my_all? { |word| word.length >= 4 } #=> false
+[nil, true, 99].my_all?                              #=> false
+
+#ANY?
+%w[ant bear cat].any? { |word| word.length >= 3 } #=> true
+%w[ant bear cat].any? { |word| word.length >= 4 } #=> true
+[nil, true, 99].any?                              #=> true
+
+#NONE?
+%w{ant bear cat}.my_none? { |word| word.length == 5 } #=> true
+%w{ant bear cat}.my_none? { |word| word.length >= 4 } #=> false
+[].my_none?                                           #=> true
+[nil].my_none?                                        #=> true
+[nil, false].my_none?                                 #=> true
+[nil, false, true].my_none?                           #=> false
+
+#COUNT
+ary = [1, 2, 4, 2]
+ary.my_count                    #=> 4
+ary.my_count(2)                 #=> 2
+ary.my_count{ |x| x%2==0 }      #=> 3
+
+#MAP
+(1..4).my_map { |i| i*i }       #=> [1, 4, 9, 16]
+(1..4).my_map { "cat"  }        #=> ["cat", "cat", "cat", "cat"]
+
+
+#INJECT
+(5..10).my_inject(:+)                             #=> 45
+(5..10).my_inject { |sum, n| sum + n }            #=> 45
+(5..10).my_inject(1, :*)                          #=> 151200
+(5..10).my_inject(1) { |product, n| product * n } #=> 151200
+longest = %w{ cat sheep bear }.my_inject do |memo, word|
+   memo.length > word.length ? memo : word
+end
+longest                                        #=> "sheep"
+
 def multiply_els(array)
     array.my_inject(:*)
 end
-
-longest = %w{ cat sheep bear }.my_inject do |memo, word|
-    memo.length > word.length ? memo : word
- end
-
- (5..10).my_inject(1, :*) 
- (5..10).my_inject(1) { |product, n| product * n }
-
-
- (5..10).my_inject { |sum, n| sum + n }
-=end
